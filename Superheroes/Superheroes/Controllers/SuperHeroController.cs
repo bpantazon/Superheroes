21b851988e20199a3dcc87c21406a9f1ff43ac9e
Ben Pantazon <pantazon-b@live.com>
2019-03-01 12:56:26 -0600

    Created SuperHeroController for Superhero, added two views of Create and Edit

---
+++ Superheroes/Superheroes/Controllers/SuperHeroController.cs
@@ -0,0 +1,97 @@
+﻿using Superheroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Superheroes.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        ApplicationDbContext db;
+        public SuperHeroController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperHero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperHero/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Create
+        [HttpPost]
+        public ActionResult Create(Superhero hero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Superhero.Add(hero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Edit/5
+        public ActionResult Edit(int id)
+        {
+           
+            return View();
+        }
+
+        // POST: SuperHero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Superhero hero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                //db.Superhero.Where()
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
